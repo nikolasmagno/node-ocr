afca4f7177f387864c385658368303835c6cba9a
Nikolas Magno <nikolasnyo.android@gmail.com>
2017-07-31 23:57:51 -0700

    Adding minimisty to get parameters

--- tesseract_test/ocr.js
+++ tesseract_test/ocr.js
@@ -1,5 +1,12 @@
+var argv = require('minimisty');
 var tesseract = require('tesseract.js');
-var filename = 'pic.jpg';
+var filename = argv['f'];
+
+if(filename == '' || filename == undefined)
+{
+  console.log('parameter  -f file not found');
+  throw new Error('Parameter  -f (file) not found');
+}
 
 tesseract.recognize(filename)
 	 .progress(function(p) { console.log('progress', p)})
